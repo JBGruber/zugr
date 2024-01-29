79ea329e95f7fa75886674ebb9e5954211f63900
JBGruber <JohannesB.Gruber@gmail.com>
2024-01-29 11:47:51 +0100

    safer parsing

--- R/parse.r
+++ R/parse.r
@@ -6,31 +6,38 @@ parse_response <- function(x) {
   end <- NULL
   duration <- NULL
   # TODO: better way to parse this?
-  purrr::map(x, function(r) {
-    purrr::map(r[["verbindungen"]], function(v) {
-      n_abschnitt <- length(v[["verbindungsAbschnitte"]])
+  out <- purrr::map(x, function(r) {
+    purrr::map(purrr::pluck(r, "verbindungen"), function(v) {
+      n_abschnitt <- length(purrr::pluck(v, "verbindungsAbschnitte"))
       tibble::tibble(
-        id = v[["tripId"]],
-        duration = v[["verbindungsDauerInSeconds"]],
-        price = v[["angebotsPreis"]][["betrag"]],
-        changes = v[["umstiegsAnzahl"]],
-        start = v[["verbindungsAbschnitte"]][[1]][["abfahrtsZeitpunkt"]],
-        end = v[["verbindungsAbschnitte"]][[n_abschnitt]][["ankunftsZeitpunkt"]]
+        id = purrr::pluck(v, "tripId"),
+        duration = purrr::pluck(v, "verbindungsDauerInSeconds"),
+        price = purrr::pluck(v, "angebotsPreis", "betrag"),
+        changes = purrr::pluck(v, "umstiegsAnzahl"),
+        start = purrr::pluck(v, "verbindungsAbschnitte", 1, "abfahrtsZeitpunkt"),
+        end = purrr::pluck(v, "verbindungsAbschnitte", n_abschnitt, "ankunftsZeitpunkt")
       )
     }) |>
       dplyr::bind_rows()
   }) |>
-    dplyr::bind_rows() |>
-    dplyr::mutate(
+    dplyr::bind_rows()
+
+  if (nrow(out) > 0) {
+    out <- out |>
+      dplyr::mutate(
       start = lubridate::force_tz(lubridate::ymd_hms(start), "Europe/Berlin"),
       end = lubridate::force_tz(lubridate::ymd_hms(end), "Europe/Berlin"),
       duration = lubridate::as.duration(duration)
     )
+  }
+
+  return(out)
+
 }
 
 get_last <- function(x) {
-  v <- utils::tail(x[["verbindungen"]], 1)
-  a <- utils::tail(v[[1]][["verbindungsAbschnitte"]], 1)
-  lubridate::ymd_hms(a[[1]][["ankunftsZeitpunkt"]]) |>
+  v <- utils::tail(purrr::pluck(x, "verbindungen"), 1)
+  a <- utils::tail(purrr::pluck(v, 1, "verbindungsAbschnitte"), 1)
+  lubridate::ymd_hms(purrr::pluck(a, 1, "ankunftsZeitpunkt")) |>
     lubridate::force_tz("Europe/Berlin")
 }
